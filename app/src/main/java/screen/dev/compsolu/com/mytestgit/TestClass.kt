b2d8a4b55e6ff1e335853abc0b9d1a52f1083833
Hridesh <hridesh.pp@gmail.com>
2018-11-18 20:56:07 +0530

    Commit in new branch

--- app/src/main/java/screen/dev/compsolu/com/mytestgit/TestClass.kt
+++ app/src/main/java/screen/dev/compsolu/com/mytestgit/TestClass.kt
@@ -2,11 +2,14 @@ package screen.dev.compsolu.com.mytestgit
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.util.Log.INFO
 
 class TestClass : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var temp : Int = 10
     }
 
 }
